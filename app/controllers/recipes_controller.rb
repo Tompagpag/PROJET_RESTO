884afcbb946965e7f4157b4e7afaf1586ebaf557
Tom Paganel <paganel.tom@gmail.com>
2022-01-04 12:55:33 +0100

    migration of recipes table to add a name

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,10 @@
 class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
   def index
     @recipes = Recipe.all
   end
 
-  def new
-    @recipe = Recipe.new
-  end
 end
