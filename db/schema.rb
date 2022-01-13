9d8c100155fea25cb75058ebd968a4d3e511b51d
Tom Paganel <paganel.tom@gmail.com>
2022-01-13 10:41:15 +0100

    create ingredients table with recipe references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_115245) do
+ActiveRecord::Schema.define(version: 2022_01_13_092453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.float "amount_for_recipe"
+    t.float "cost_for_recipe"
+    t.float "amount_of_order"
+    t.float "cost_of_order"
+    t.string "unit_for_recipe"
+    t.string "unit_of_order"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.float "cost"
@@ -34,4 +48,5 @@ ActiveRecord::Schema.define(version: 2022_01_04_115245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "recipes"
 end
