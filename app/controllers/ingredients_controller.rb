06e3dbe635f31082844e78c2b28fa7a9151c5030
Tom Paganel <paganel.tom@gmail.com>
2022-01-18 20:47:59 +0100

    start ingredient ctrl

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,23 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name,
+                                       :amount_for_recipe,
+                                       :cost_for_recipe,
+                                       :amount_of_order,
+                                       :cost_of_order,
+                                       :unit_for_recipe,
+                                       :unit_of_order)
+  end
 end
